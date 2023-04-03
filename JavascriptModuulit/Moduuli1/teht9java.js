5f3790de4968c0ddf6300cf259ae899be0b0b277
ainoruo <aino.ruohola@metropolia.fi>
2023-04-03 09:20:34 +0300

    muokattu

--- JavascriptModuulit/Moduuli1/teht9java.js
+++ JavascriptModuulit/Moduuli1/teht9java.js
@@ -1,7 +1,7 @@
 'use strict';
 const numero = parseInt(prompt('Anna numero:'));
 if (numero < 2) {
-  document.querySelector('#numero1').innerHTML = 'Luku ei ole kantaluku.';
+  document.querySelector('#numero1').innerHTML = 'Luku ei ole alkuluku.';
 } else {
   let isPrime = true;
   for (let i = 2; i <= Math.sqrt(numero); i++) {
@@ -11,9 +11,9 @@ if (numero < 2) {
     }
   }
   if (isPrime) {
-    document.querySelector('#numero2').innerHTML = 'Luku on kantaluku.';
+    document.querySelector('#numero2').innerHTML = 'Luku on alkuluku.';
   } else {
-    document.querySelector('#numero3').innerHTML = 'Luku ei ole kantaluku.';
+    document.querySelector('#numero3').innerHTML = 'Luku ei ole alkuluku.';
   }
 }
 
